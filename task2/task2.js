11ce75f6d4ec6f256be568032d1eb34b6f80bdc3
srihari797 <sriharik1707@gmail.com>
2025-11-09 14:45:46 +0530

    Create task2.js

---
+++ task2/task2.js
@@ -0,0 +1,13 @@
+const a=10;
+const b=5;
+const  c=a+b;
+console.log(c);
+if(a>b){
+    console.log("a is greate than b");
+}
+else if(a==b){
+    console.log("a equals b");
+}
+else{
+    console.log("b is greater than c")
+}
